88526b20d718708a0f0527e967a86bb5195c8974
aman162000 <borse.aman@rediffmail.com>
2021-07-14 22:58:44 +0530

    Templates and Staticfiles added.

--- fakeEcomAPIs/urls.py
+++ fakeEcomAPIs/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('api/',include('api.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
