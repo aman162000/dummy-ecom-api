c37b5177da867972bdcdaeccee2723e97fa4004d
aman162000 <borse.aman@rediffmail.com>
2021-07-20 17:39:11 +0530

    Default JSONRenderer is set.

--- fakeEcomAPIs/settings.py
+++ fakeEcomAPIs/settings.py
@@ -60,9 +60,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
-    # 'DEFAULT_RENDERER_CLASSES': (
-    #     'rest_framework.renderers.JSONRenderer',
-    # )
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
     # 'DEFAULT_PERMISSION_CLASSES': (
     #     "rest_framework_api_key.permissions.HasAPIKey",
     #      )
